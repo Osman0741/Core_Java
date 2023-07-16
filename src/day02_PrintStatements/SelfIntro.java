c3736973ca99d321c0626ac86771bdf8fbcf4348
LENOVO <osmnozdmrosh@gmail.com>
2023-07-16 13:24:38 +0300

    Print Statement

---
+++ src/day02_PrintStatements/SelfIntro.java
@@ -0,0 +1,15 @@
+package day02_PrintStatements;
+
+public class SelfIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Cydeo!");
+        System.out.println("My name is: Osman");
+        System.out.println("I am from: Turkey");
+        System.out.println("I am currently living in: Kocaeli");
+        System.out.println("My favorite book is: Java");
+        System.out.println("My favorite TV show is: None");
+        System.out.println("My favorite sport is: Soccer");
+        System.out.println("My dream job is: Java Developer");
+    }
+}
