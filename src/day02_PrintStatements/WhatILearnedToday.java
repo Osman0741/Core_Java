76096a094a1457025357a46fa2bda18c399f1c69
LENOVO <osmnozdmrosh@gmail.com>
2023-07-16 13:27:14 +0300

    Print Statement Java Components

---
+++ src/day02_PrintStatements/WhatILearnedToday.java
@@ -0,0 +1,19 @@
+package day02_PrintStatements;
+
+public class WhatILearnedToday {
+    public static void main(String[] args) {
+
+        System.out.println("Question 1:What are JRE and Java compiler used for?");
+        System.out.println("Answer 1: JRE is the system library that contains the resources when needed  while Javac is a program that translates Java codes into bytecode that the computer can understand");
+        System.out.println("Question 2:What is the main method used for");
+        System.out.println("Answer 2: Main Method is used to run codes. It is where program starts to run.");
+        System.out.println("Question 3:What is the src folder used for?");
+        System.out.println("Answer 3: The src folder is used for storing packages");
+        System.out.println("Question 4:What is the package used for?");
+        System.out.println("Answer 4:The Package is used for storing java files");
+        System.out.println("Question 5:What is the class used for");
+        System.out.println("Answer 5:The Class is used for writing Java codes");
+        System.out.println("Question 6:What is System.out.println  is used for?");
+        System.out.println("Answer 6: It is used for printing data on the console");
+    }
+}
