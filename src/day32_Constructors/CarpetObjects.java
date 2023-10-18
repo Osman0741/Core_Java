4971b158a46e474ebdc5aef745f5a4cd509f6afd
LENOVO <osmnozdmrosh@gmail.com>
2023-10-18 14:26:55 +0300

    Constructor

---
+++ src/day32_Constructors/CarpetObjects.java
@@ -0,0 +1,11 @@
+package day32_Constructors;
+
+public class CarpetObjects {
+    public static void main(String[] args) {
+        Carpet carpet1= new Carpet(5.5,4.5,10,true);
+        System.out.println(carpet1);
+
+        Carpet carpet2 = new Carpet(10, 20, 10, false);
+        System.out.println(carpet2);
+    }
+}
